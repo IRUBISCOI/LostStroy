48e0f94419ff086c20dfd1c957fe2d939d4db374
IRUBISCOI <gocsh1004@gmail.com>
2022-12-08 19:57:59 +0900

    Project Default Setting
    # Remove Resource directory
    /Content/BloodFX
    /Content/BossAIToolkit
    /Content/GothicTemple
    /Content/GrruzamSwordPack
    /Content/HugeAnimBundle
    /Content/MuzzleFlashesVol1
    /Content/NiagaraDashEffects
    /Content/Sword_Slashes
    /Content/StarterContent
    /Content/Mannequin
    /Content/ThirdPerson
    /Content/ThirdPersonBP
    /Content/Geometry
    
    Add Asset & StartcontentsPack & ThirdPersonKit

---
+++ Source/LostStroy/LostStroyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LostStroyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOSTSTROY_API ALostStroyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
